68b1de98d7d871d168c95a9d50a7c107bf5e80cb
gchure <griffinchure@gmail.com>
2023-07-26 21:56:06 -0700

    Tweak to dev script

--- sandbox/development.py
+++ sandbox/development.py
@@ -11,12 +11,17 @@ imp.reload(hplc.quant)
 
 
 # Load the simulated data and ground truth
-data = pd.read_csv('./simulated_chromatogram.csv')
-peaks = pd.read_csv('./simulated_chromatogram_peaks.csv') 
+data = pd.read_csv('./sample_chromatogram.txt')
 chrom = hplc.quant.Chromatogram(data, cols={'time':'time_min','signal':'intensity_mV'})
+chrom.crop([10, 20])
 chrom.fit_peaks()
 chrom.show()
 
 
 #%%
-data
+plt.plot(np.diff(data['intensity_mV']))
+
+#%%
+_df = chrom.df
+f = np.sum(chrom.deconvolved_peaks, axis=1)
+len(chrom.window_props)
