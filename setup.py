72d752f694580fdd46bcf354358d2effc7511c25
gchure <griffinchure@gmail.com>
2023-09-27 14:44:09 -0700

    Preparing tweaks for next PyPi deploy

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import pathlib
 
-__version__ = "0.2.00"
+__version__ = "0.2.10"
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
