483001475c2a8880cd347cf4b817fc40f7502577
gchure <griffinchure@gmail.com>
2024-02-09 15:25:57 -0800

    Bumb version for archival

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import pathlib
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
