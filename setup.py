d562fd8b2cd425b18d6a7df358f2118f01a687ed
gchure <griffinchure@gmail.com>
2026-06-25 22:13:33 -0700

    Bump version to 0.2.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import pathlib
 
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
